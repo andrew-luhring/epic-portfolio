21547ec5fabe2cbcd86a95c0857b24aa416ce93c
andrew-luhring <andrew.luhring@gmail.com>
2014-03-20 16:53:23 -0500

    adds spike to public/tests/_portfolio.js

--- public/tests/_portfolio.js
+++ public/tests/_portfolio.js
@@ -1,14 +1,10 @@
 /* jshint undef: true */
 /* global jQuery: true, resizeImg: true, Shadowbox: true, port: true, it: true*/
-
 "use strict";
-
 var expect = require("expect.js");
 var $ = require('../lib/jquery');
 var jsdom = require("jsdom").jsdom;
 var doc = jsdom('');
-
-
 
 
 
@@ -21,24 +17,18 @@ var doc = jsdom('');
 
 	//port = root.port || {};
 
+describe("thing", function(){
+	it("is a passing test", function(){
+		expect("this is").to.be.an("object");
+	});
+
+});
 
 //(function(root){
 
 
 //
-// 	root.port = root.port || {};
-
-
-describe("expect", function(){
-	it("exists", function(){
-		expect("a").to.be.ok();
-	});
-	it('uses jquery', function(){
-
-		expect($).to.be.ok();
-	});
-
-});
+// 	root.port = root.port || {};e
 
 
 //})(this);
